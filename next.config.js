d16c1fa4faeca3d5b8ccddc18ab819726f8daea3
Eber Palma <al221911577@gmail.com>
2023-05-23 18:19:40 -0600

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: false,
+    env: {
+        MYSQL_HOST: "127.0.0.1",
+        MYSQL_PORT: "3306",
+        MYSQL_DATABASE: "orange_tree",
+        MYSQL_USER: "root",
+        MYSQL_PASSWORD: "",
+    },
+};
